80b7b74fac2354d15716bf93ec7c4e4611155eba
YunMin-Song <thd930308@naver.com>
2020-08-23 17:18:30 +0900

    Error Position Setting Complete

--- rayout.js
+++ rayout.js
@@ -3,6 +3,7 @@ const ejs = require("ejs");
 const bodyParser = require('body-parser');
 const app = express();
 
+// Network connect Area
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));  // URL 인코딩 안함
 app.use(bodyParser.json());                         // JSON 타입으로 파싱하게 설정
@@ -13,36 +14,42 @@ const server = app.listen(2000, function () {
     console.log("Express server has started on port 2000")
 });
 
-let errorlog;
+// 미리 지정한 에러
+const ERROR = ["NullPointerException", "IllegalAccessException", "InstantiationException", "StackOverflowError",
+"IndexOutOfBoundException"];
+let errorLog;               // Error Log 전체를 출력할 변수
+let errorHead;              // Error Name 을 출력할 변수
+let errorQueue;             // Error Queue 에 포함될 변수
 
+// 파일을 읽으면서 구문 분석 진행 함
 function fileRead() {
-// TODO : 에러 로그 분산 DB 사용해서 딜레이 줄여야 함
     let fs = require('fs');
     fs.readFile('log.txt', 'utf-8', function (err, data) {
-            errorlog = data;
-            let name = errorlog.indexOf("NullPointerException");
-            const errorTest = "NullPointerException";
-            // TODO :
-            console.log("에러 길이 : "+errorTest.length);
-            console.log(name);
-            let test = errorlog.substring(name,name+errorTest.length);
-            console.log(test);
-
+            errorLog = data;
+            // TODO : 에러 부분 크롤링 필요
+            // TODO : 크롤링 이후 현재 로그에 있는 값 분석, Web 출력
+        // for(let i=0;i<ERROR.length;i++) {
+        //     // TODO : 2중 for 이용, 에러 분석
+        //     let name = errorLog.indexOf("NullPointerException");
+        // }
+        let name = errorLog.indexOf("NullPointerException");
+        const errorTest = "NullPointerException";
+        // console.log("에러 길이 : "+errorTest.length);
+            // console.l og(name);
+            errorHead = errorLog.substring(name,name+errorTest.length);
+            console.log(errorHead);
         }
     );
 
-    console.log(errorlog);
+    console.log(errorLog);
 // 로그 출력
 // TODO : 로그를 로딩 하는 시간이 오래 걸림
     app.get("/", function (req, res) {
         console.log("Web Page Upload");
-        res.render("index", {errorlog: errorlog});
+        // 각 위치에 Set함 속성은 Key:Value
+        res.render("index", {errorlog: errorLog, errorQueue:errorHead,errorHead:errorHead});
     });
-    // 5초 뒤 setTimeout 메소드 실행
-    setTimeout(NameSearch, 5000, errorlog);
 }
-
-// TODO : Log를 파싱, 구문분석을 진행해서 각각에 위치로 Set 되는 메소드 정의 필요
 function NameSearch(errorlog) {
     //let ch = errorLog.indexOf("NullPointerException");
     console.log(errorlog);
