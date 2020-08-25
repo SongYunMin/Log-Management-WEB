50deebd2c85d54238ecdcda8d8a75581525b6a2f
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-08-25 16:53:16 +0900

    Synchronous file Read using read method

--- rayout.js
+++ rayout.js
@@ -18,9 +18,19 @@ const server = app.listen(2000, function () {
 const ERROR = ["NullPointerException", "IllegalAccessException", "InstantiationException", "StackOverflowError",
     "IndexOutOfBoundException"];
 let errorLog;               // Error Log 전체를 출력할 변수
-let errorHead;              // Error Name 을 출력할 변수
-let errorSection;
+let errorHead = [];              // Error Name 을 출력할 변수
+let errorSection = [];
 let errorQueue;             // Error Queue 에 포함될 변수
+
+// '동기' readFileSync , '비동기' readFile
+errorLog = fs.readFileSync('log.txt', 'utf-8', function (err, data) {});
+for(let i=0;i<ERROR.length;i++){            // 에러가 담겨있는 배열의 길이
+    let name = errorLog.indexOf(ERROR[i]);  // 파일에서 에러의 위치를 찾고
+    console.log(name);
+    errorHead[i] = errorLog.substring(name, name+ERROR[i].length);
+    errorSection[i] = errorLog.substring(name,name+3000);
+    console.log(errorHead[i]);
+}
 
 // 로그 출력
 // TODO : 로그를 로딩 하는 시간이 오래 걸림
@@ -30,36 +40,7 @@ app.get("/", function (req, res) {
     res.render("index", {errorlog: errorSection, errorQueue: ERROR, errorHead: errorHead});
 });
 
-let file = fs.readFile('log.txt', 'utf-8', function (err, data) {
-        errorLog = data;
-        // for(let i=0;i<ERROR.length;
-    // i++) {
-        //     // TODO : 2중 for 이용, 에러 분석
-        //     let name = errorLog.indexOf("NullPointerException");
-        // }
-        let name = errorLog.indexOf("NullPointerException");
-        const errorTest = "NullPointerException";
-
-        errorHead = errorLog.substring(name, name + errorTest.length);
-        errorSection = errorLog.substring(name, name + 3000);
-        console.log(errorHead);
-    }
-);
-
-// 파일을 읽으면서 구문 분석 진행 함
-function fileRead() {
-    const express = require("express");
-    const app = express();
-
-}
-
 function button_click() {
-    // Network connect Area
-    app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({extended: false}));  // URL 인코딩 안함
-    app.use(bodyParser.json());                         // JSON 타입으로 파싱하게 설정
-    app.use(express.static(__dirname + '/'));
-    // 로그 출력
 // TODO : 로그를 로딩 하는 시간이 오래 걸림
     app.get("/", function (req, res) {
         console.log("Web Page Upload");
