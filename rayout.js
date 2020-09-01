02742a31df23e564dd8fd94d2c61499c490e56b7
Song-YunMin <48168717+SongYunMin@users.noreply.github.com>
2020-09-01 15:13:46 +0900

    Sorting in progress

--- rayout.js
+++ rayout.js
@@ -14,11 +14,18 @@ const server = app.listen(2000, function () {
 });
 
 // Member Field Area
-var ERROR = ["NullPointerException", "IllegalAccessException", "InstantiationException", "StackOverflowError",
+let ERROR = ["NullPointerException", "IllegalAccessException", "InstantiationException", "StackOverflowError",
     "IndexOutOfBoundException"];
-var errorInit;
-var errorHead = [];         // Error Name 을 출력할 변수
-var errorSection = [];
+let referenceDoc = ["https://developer.android.com/reference/java/lang/NullPointerException?hl=en",
+    "https://developer.android.com/reference/java/lang/IllegalAccessException",
+    "https://developer.android.com/reference/java/lang/InstantiationException?hl=en",
+    "https://developer.android.com/reference/java/lang/StackOverflowError?hl=en",
+    "https://developer.android.com/reference/java/lang/IndexOutOfBoundsException?hl=en"];
+let errorHeadInit;
+let errorDocInit;
+let errorHead = [];         // Error Name 을 출력할 변수
+let errorSection = [];
+let errorDoc = [];
 
 // Main Method
 function main() {
@@ -29,16 +36,17 @@ function main() {
         let name = errorLog.indexOf(ERROR[i]);  // In File Error Detection
         if (name !== -1) {
             errorHead[i] = errorLog.substring(name, name + ERROR[i].length);
+            errorDoc[i] = referenceDoc[i];
             let buf = errorLog.substring(name, name + 3000);
             let emptyCheck = 0, emptyCheckBuf = 0;
 
-            while(true){
-                emptyCheckBuf = buf.indexOf('AndroidRuntime',emptyCheck+14);
+            while (true) {
+                emptyCheckBuf = buf.indexOf('AndroidRuntime', emptyCheck + 14);
                 // -1 emptyCheck 대입 됨
-                if(emptyCheckBuf === -1){
-                    let escape = errorLog.indexOf("\n",emptyCheck);
+                if (emptyCheckBuf === -1) {
+                    let escape = errorLog.indexOf("\n", emptyCheck);
                     // for(let j=(name+emptyCheck); j<)
-                    errorSection[i] = errorLog.substring(name, name+escape);
+                    errorSection[i] = errorLog.substring(name, name + escape);
                     console.log("start : " + name);
                     console.log("end(escape) : " + escape);
                     console.log("Log : " + errorSection[i]);
@@ -48,8 +56,14 @@ function main() {
             }
         }
     }
-    errorInit = errorHead[0];
+    errorHeadInit = errorHead[0];
+    errorDocInit = errorDoc[0];
+
+
+
 }
+
+
 
 // 로그 출력
 // TODO : 로그를 로딩 하는 시간이 오래 걸림
@@ -59,7 +73,9 @@ app.get("/", function (req, res) {
     res.render("index.ejs", {
         errorSection: errorSection,
         errorHead: errorHead,
-        errorInit: errorInit
+        errorHeadInit: errorHeadInit,
+        errorDoc: errorDoc,
+        errorDocInit: errorDocInit
     });
     res.end();
 });
